1978f0e79ca4eaa1689ffc8655f5f76732ebb3ac
talrotshtein <talrotshtein@campus.technion.ac.il>
2022-05-20 12:02:22 +0300

    created new files

---
+++ HealthPoints.h
@@ -0,0 +1,10 @@
+//
+// Created by Tal on 5/20/2022.
+//
+
+#ifndef EX3_HEALTHPOINTS_H
+#define EX3_HEALTHPOINTS_H
+
+
+
+#endif //EX3_HEALTHPOINTS_H
---
+++ Queue.h
@@ -0,0 +1,8 @@
+//
+// Created by Tal on 5/20/2022.
+//
+
+#ifndef EX3_QUEUE_H
+#define EX3_QUEUE_H
+
+#endif //EX3_QUEUE_H
