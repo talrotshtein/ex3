c6adf71e14cc3e569742f368d155a090f7b626bc
shahar.ben <shahar.ben@campus.technion.ac.il>
2022-05-20 13:49:38 +0300

    added arithmatic operators

--- HealthPoints.cpp
+++ HealthPoints.cpp
@@ -26,6 +26,54 @@ HealthPoints& HealthPoints::operator=(const HealthPoints& HP) {
     return *this;
 }
 
+HealthPoints& HealthPoints::operator+=(int HPtoAdd)
+{
+    if (this->m_HP + HPtoAdd > this->m_maxHP)
+    {
+        this->m_HP = this->m_maxHP;
+        return HealthPoints;
+    }
+    else if (this->m_HP + HPtoAdd <=0)
+    {
+        this->m_HP = 0;
+        return HealthPoints;
+    }
+    this->m_HP = this->m_HP + HPtoAdd;
+    return HealthPoints;
+}
+
+HealthPoints& HealthPoints::operator-(HealthPoints& currentHp , int HpToRemove)
+{
+    if (currentHp->m_HP - HPtoRemove > currentHp->m_maxHP)
+    {
+        currentHp->m_HP = currentHp->m_maxHP;
+        return currentHp;
+    }
+    else if (currentHp->m_HP - HPtoRemove <=0)
+    {
+        currentHp->m_HP = 0;
+        return currentHp;
+    }
+    currentHp->m_HP = currentHp->m_HP - HPtoRemove;
+    return currentHp;
+}
+
+HealthPoints& HealthPoints::operator-=(int HPtoRemove)
+{
+    if (this->m_HP - HPtoRemove > this->m_maxHP)
+    {
+        this->m_HP = this->m_maxHP;
+        return HealthPoints;
+    }
+    else if (this->m_HP - HPtoRemove <=0)
+    {
+        this->m_HP = 0;
+        return HealthPoints;
+    }
+    this->m_HP = this->m_HP - HPtoRemove;
+    return HealthPoints;
+}
+
 bool operator==(const HealthPoints& hp1, const HealthPoints& hp2){
     return hp1.m_HP == hp2.m_HP;
 }
