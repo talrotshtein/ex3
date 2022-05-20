0a7d999fe2708abea13fd2a7c300f06f017c8d81
talrotshtein <talrotshtein@campus.technion.ac.il>
2022-05-20 12:25:26 +0300

    created HP.h

--- HealthPoints.h
+++ HealthPoints.h
@@ -5,6 +5,33 @@
 #ifndef EX3_HEALTHPOINTS_H
 #define EX3_HEALTHPOINTS_H
 
+class HealthPoints {
+    int m_maxHP;
+    int m_HP;
+public:
+    HealthPoints(int maxHP = 100);
 
+    HealthPoints &operator-=(int HPtoRemove);
+
+    HealthPoints &operator+=(int HPtoAdd);
+
+    HealthPoints &operator+(int HPtoAdd);
+
+    HealthPoints &operator-(int HPtoRemove);
+
+    bool operator>=(const HealthPoints& hp1, const HealthPoints& hp2) const;
+
+    bool operator>(const HealthPoints& hp1 , const HealthPoints& hp2) const;
+
+    bool operator<=(const HealthPoints& hp1, const HealthPoints& hp2) const;
+
+    bool operator<(const HealthPoints& hp1 , const HealthPoints& hp2) const;
+
+    bool operator==(const HealthPoints& hp1 , const HealthPoints& hp2) const;
+
+    bool operator!=(const HealthPoints& hp1 , const HealthPoints& hp2) const;
+
+    friend ostream& operator<<(ostream&, const HealthPoints&);
+}
 
 #endif //EX3_HEALTHPOINTS_H
