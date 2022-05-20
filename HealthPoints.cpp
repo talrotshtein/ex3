2b81ffcad369f39aa481d7f3d97bc0562310c359
talrotshtein <talrotshtein@campus.technion.ac.il>
2022-05-20 13:37:22 +0300

    working on hp.cpp

--- HealthPoints.cpp
+++ HealthPoints.cpp
@@ -3,10 +3,53 @@
 //
 #include "HealthPoints.h"
 
-HealthPoints::HealthPoints(int maxHP):
-    m_HP(maxHP),
-    m_maxHP(maxHP)
-{}
+HealthPoints::HealthPoints(int maxHP){
+    if(maxHP <= 0)
+    {
+        throw InvalidArgument();
+    }
+    this->m_maxHP = maxHP;
+    this->m_HP = maxHP;
+}
 
+HealthPoints::HealthPoints(const HealthPoints& HP){
+    this->m_HP = HP.m_HP;
+    this->m_maxHP = HP.m_maxHP;
+}
 
+HealthPoints& HealthPoints::operator=(const HealthPoints& HP) {
+    if(this == &HP){
+        return *this;
+    }
+    this->m_HP = HP.m_HP;
+    this->m_maxHP = HP.m_maxHP;
+    return *this;
+}
 
+bool operator==(const HealthPoints& hp1, const HealthPoints& hp2){
+    return hp1.m_HP == hp2.m_HP;
+}
+
+bool operator<(const HealthPoints& hp1, const HealthPoints& hp2){
+    return hp1.m_HP < hp2.m_HP;
+}
+
+bool operator<=(const HealthPoints& hp1, const HealthPoints& hp2){
+    return !(hp2 < hp1);
+}
+
+bool operator>=(const HealthPoints& hp1, const HealthPoints& hp2){
+    return !(hp1 < hp2);
+}
+
+bool operator>(const HealthPoints& hp1, const HealthPoints& hp2){
+    return hp2<hp1;
+}
+
+bool operator!=(const HealthPoints& hp1, const HealthPoints& hp2){
+    return !(hp2 == hp1);
+}
+
+std::ostream& operator<<(std::ostream& os, const HealthPoints& HP){
+    return os<<HP.m_HP<<"("<<HP.m_maxHP<<")";
+}
--- HealthPoints.h
+++ HealthPoints.h
@@ -4,13 +4,17 @@
 
 #ifndef EX3_HEALTHPOINTS_H
 #define EX3_HEALTHPOINTS_H
-#include <ostream>
+#include <ostream>//check about ostream
 
 class HealthPoints {
     int m_maxHP;
     int m_HP;
 public:
     HealthPoints(int maxHP = 100);
+
+    HealthPoints(const HealthPoints& HP);
+
+    HealthPoints& operator=(const HealthPoints&);
 
     HealthPoints &operator-=(int HPtoRemove);
 
@@ -24,7 +28,10 @@ public:
 
     friend bool operator==(const HealthPoints &hp1, const HealthPoints &hp2);
 
-    friend std::ostream& operator<<(std::ostream&, const HealthPoints&);
+    friend std::ostream& operator<<(std::ostream&, const HealthPoints&);//check about ostream
+
+    class InvalidArgument {};
+
 };
 
     bool operator>=(const HealthPoints& hp1, const HealthPoints& hp2);
